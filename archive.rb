eef29335f0ef9c3f4ac22abc578bc2b46b17e11c
Blake Taylor <btaylor@agoragames.com>
2011-01-06 11:05:23 -0500

    Change options to move and junk.

--- archive.rb
+++ archive.rb
@@ -2,11 +2,11 @@ require 'fileutils'
 move_to_dir = ARGV[0] || Time.now.year.to_s
 Dir.foreach(Dir.pwd) do |entry|
   unless entry =~ /^\.|^[\d]{4}|#{move_to_dir}/
-    print "#{entry} (skip/move/trash): "
+    print "#{entry} (move/junk): "
     case gets.chomp
     when /^m(ove)?$/
       FileUtils.mv entry, move_to_dir
-    when /^t(rash)?$/
+    when /^j(unk)?$/
       FileUtils.mv entry, '.junk'
     end
   end
