288196cdd031bff6edc8810526521e5c465ec6f5
Blake Taylor <btaylor@agoragames.com>
2011-01-06 11:20:15 -0500

    Add usage that is printed when the script is run.

--- archive.rb
+++ archive.rb
@@ -1,8 +1,22 @@
 require 'fileutils'
 move_to_dir = ARGV[0] || Time.now.year.to_s
+
+puts <<usage
+
+Project Archiver
+======================================================================
+
+Tell me what to do with each entry. Enter nothing to skip.
+  - Entries which you move will go to #{move_to_dir}.
+  - Entries which you junk will for to .junk.
+
+ctrl-c to exit at any time.
+
+usage
+
 Dir.foreach(Dir.pwd) do |entry|
   unless entry =~ /^\.|^[\d]{4}|#{move_to_dir}/
-    print "#{entry} (move/junk): "
+    print "#{entry} (move/junk) or skip: "
     case gets.chomp
     when /^m(ove)?$/
       FileUtils.mv entry, move_to_dir
