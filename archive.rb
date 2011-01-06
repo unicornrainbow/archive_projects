04965fed5449cb7fc9c60fb044e8e3974399412d
Blake Taylor <btaylor@agoragames.com>
2011-01-06 10:56:05 -0500

    Ignore hidden files and directory and default archive named directories.

--- archive.rb
+++ archive.rb
@@ -1,7 +1,7 @@
 require 'fileutils'
 move_to_dir = ARGV[0] || Time.now.year.to_s
 Dir.foreach(Dir.pwd) do |entry|
-  unless [move_to_dir, '.', '..'].include?(entry)
+  unless entry =~ /^\.|^[\d]{4}|#{move_to_dir}/
     print "#{entry} (skip/move/trash): "
     case gets.chomp
     when /^m(ove)?$/
